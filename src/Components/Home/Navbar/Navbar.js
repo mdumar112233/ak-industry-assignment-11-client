4ef644255a972c2fc7cbd90835af66bbaab385ef
md umar faruk <mdumar11233>
2021-04-15 02:19:01 +0600

    added new data

---
+++ src/Components/Home/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../image/f9e48bbcf7aca98f0eff903681e84a2b.png';
+import './Header.css';
+
+const Navbar = () => {
+    return (
+        <div  className='row header-container'>
+            <div className="col-md-4 logo-image">
+                <img src={logo} alt=""/>
+            </div>
+            <div className="col-md-8 menu-link">
+                <ul>
+                    <li>
+                        <Link className='link' to='/home'>Home</Link>
+                    </li>
+                    <li>
+                        <Link className='link' to='/project'>Project</Link>
+                    </li>
+                    <li>
+                        <Link className='link' to='/admin'>Admin</Link>
+                    </li>
+                    <li>
+                        <Link className='link'>About us</Link>
+                    </li>
+                    <li>
+                        <Link className='link'>Contact</Link>
+                    </li>
+                    <li>
+                        <Link className='link' to='/login'>Login</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
