14c6157500cf448b9f60e83e66f90739932f9ba6
md umar faruk <mdumar11233>
2021-04-18 01:03:03 +0600

    added new data

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
       <Router>
-        <p>email: {loggedInUser.email}</p>
       <Switch>
         <Route exact path='/'>
           <Home/>
--- src/Components/Admin/MakeAdmin/MakeAdmin.js
+++ src/Components/Admin/MakeAdmin/MakeAdmin.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdminSidebar from '../AdminSidebar/AdminSidebar';
 import './MakeAdmin.css';
 
 const MakeAdmin = () => {
+    const [admin, setAdmin] = useState({});
+    console.log(admin);
+    const handleChange = (e) => {
+        const newInfo = {...admin};
+        newInfo[e.target.name] = e.target.value;
+        setAdmin(newInfo);
+    }
+
+    const handleAdmin = (e) => {
+        const adminData = {...admin};
+        fetch('http://localhost:5000/addAdmin', {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(adminData)
+        })
+        .then(data => {
+            console.log('admin addedd');
+        })
+        e.preventDefault();
+    }
 
     return (
         <div className='row'>
@@ -10,8 +30,10 @@ const MakeAdmin = () => {
             <div className="col-md-10">
                 <div className="mt-5 ms-4">
                     <h3>Make Admin</h3>
+                    <form action="">
                     <label className='mt-3' htmlFor="">Email</label> <br/>
-                    <input onBlur={handleAdmin} type="email" placeholder='Enter your email'/> <input className='make-admin-btn' type="submit" value='Submit'/>
+                    <input onBlur={handleChange} type="email" name='email' placeholder='Enter your email' required/> <input onClick={handleAdmin} className='make-admin-btn' type="submit" value='Submit'/>
+                    </form>
                 </div>
             </div>
         </div>
