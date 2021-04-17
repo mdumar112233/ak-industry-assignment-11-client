34513120c47f8680566273f89949d2fc3f82d854
md umar faruk <mdumar11233>
2021-04-17 17:08:52 +0600

    added new data

--- src/Components/UserDasboard/Book/Book.js
+++ src/Components/UserDasboard/Book/Book.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import PaymentCard from '../PaymentCard/PaymentCard';
 import { useParams } from 'react-router';
+import { UserContext } from '../../../App';
 
 const stripePromise = loadStripe('pk_test_51IeAn5F8Tt6idX1YNJvJVrIfNjU3XrD6sHsAdImNegbHw4cdba24Ps7RmrvZ0hrI3uVrTVxzgyhI6SxkDlPcs66700yOJaMdXL');
 
-const Book = () => {
+const Book = ({handlePayment}) => {
     const {id} = useParams();
     const [serviceInfo, setServiceInfo] = useState([]);
-    const [isUserData, setIsUserData] = useState({
-        isData: false
-    });
+    const [isUserData, setIsUserData] = useState({isData: false});
     console.log(isUserData);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
     const handleChange = e => {
         const newData = {...isUserData};
         newData[e.target.name] = e.target.value;
         newData.isData = true;
-        console.log(newData);
+        setIsUserData(newData);
     }
 
     useEffect(() => {
@@ -28,31 +28,32 @@ const Book = () => {
         .then(data => setServiceInfo(data))
     }, [id])
 
+    console.log(handlePayment);
+
+    const handleUserInfo = () => {
+        console.log(handlePayment);
+    }
+
     return (
         <div className='row'>
             <Sidebar></Sidebar>
             <div className="col-md-10 mt-5 ml-5" style={{position: 'absolute', right: 0}}>
                 <div className="ms-4">
-                    {
-                        isUserData ?
                         <div>
                         <h3>Book</h3>
-                        <input onBlur={handleChange} name='name' className='w-50' type="text" placeholder='Enter your name'/><br/>
-                        <input onBlur={handleChange} name='email' className='mt-3 mb-3 w-50' type="text" placeholder='Enter your email'/><br/>
+                        <input onSubmit={handleChange} name='name' className='w-50' type="text" value={loggedInUser.name} placeholder='Enter your name'/><br/>
+                        <input onSubmit={handleChange} name='email' className='mt-3 mb-3 w-50' value={loggedInUser.email} type="email" placeholder='Enter your email'/><br/>
                         {
-                            serviceInfo.map(service => <input onBlur={handleChange} name='service' className='w-50' type="text" placeholder='Enter your service' value={service.name}/>)
-                        }
-                        <button onClick={() => setIsUserData(!isUserData)}>Submit</button>
-                        </div> : ''
-                    }
-                    {
-                        isUserData ? '' :
+                            serviceInfo.map(service => <input onSubmit={handleChange} name='service' className='w-50' type="text" placeholder='Enter your service' value={service.name}/>)
+                        } <br/>
+                        <button onClick={handleUserInfo}>submit</button>
+                        </div>
                         <div className='w-50 mt-3'>
                         <Elements stripe={stripePromise}>
-                            <PaymentCard></PaymentCard>                   
+                            <PaymentCard handlePayment={handlePayment}></PaymentCard>                   
                         </Elements>
                     </div> 
-                    }
+                    
                 </div>
             </div>
         </div>
--- src/Components/UserDasboard/PaymentCard/PaymentCard.js
+++ src/Components/UserDasboard/PaymentCard/PaymentCard.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
 import './PaymentCard.css';
 
-const PaymentCard = () => {
+const PaymentCard = ({handlePayment}) => {
   const stripe = useStripe();
   const elements = useElements();
 
   const [paymentError, setPaymentError] = useState(null);
   const [paymentSuccess, setPaymentSuccess] = useState(null);
-
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -22,20 +22,43 @@ const PaymentCard = () => {
       card: cardElement,
     });
 
+    console.log(paymentMethod);
     if (error) {
       setPaymentError(error.message);
       setPaymentSuccess(null)
     } else {
       setPaymentSuccess(paymentMethod.id);
+      handlePayment(paymentMethod.id);
       setPaymentError(null);
     }
+    // console.log(userData);
+    // if(null){
+    //   const userInfoData = {...userData, payId:  paymentSuccess.id}
+    //   console.log(userInfoData);
+    //   fetch('http://localhost:5000/userInfo', {
+    //     method: 'POST',
+    //     headers: {'content-type': 'application/json'},
+    //     body: JSON.stringify(userInfoData)
+    //   })
+    //   .then(res => {
+    //     console.log('data send successfully');
+    //   })
+    // }
+
   };
+
+  // const handleUserData = () => {
+  //   if(null){
+
+  //   }
+  // }
+
 
   return (
     <div>
           <form onSubmit={handleSubmit}>
             <CardElement />
-            <button type="submit" disabled={!stripe} className='pay-btn'>
+             <button type="submit" disabled={!stripe} className='pay-btn'>
               Pay
             </button>
           </form>
