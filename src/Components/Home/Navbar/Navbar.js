53de0ee25f70c27693bafd13ea08376bc1d50c3b
md umar faruk <mdumar11233>
2021-04-18 19:45:04 +0600

    added new data

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../image/f9e48bbcf7aca98f0eff903681e84a2b.png';
+import menu from '../../../image/menu.png';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
     return (
         <div className='row navbar-container'>
             <div className="col-md-4 logo-image">
-                <img src={logo} alt="logo"/><span className='logo'>AK INDUSTRY</span>
+                <div className="logoOne"><img src={logo} alt="logo"/><span className='logo'>AK INDUSTRY</span></div>
+                <div className='menu-icon'><img onClick={() => setS} src={menu} alt=""/></div>
             </div>
-            <div className="col-md-8 menu-link">
+            <div className="col-md-8 menu-link" id={showLinks ? 'hidden': ''}>
                 <ul>
                     <li>
                         <Link className='link' to='/home'>Home</Link>
